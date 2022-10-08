1658b13db8dbe769a84146afea8b299fc2cc0988
Leonardo Ramos <leosaor@outlook.com>
2022-10-07 21:37:48 -0300

    Adiciona novas rotas get, put e delete

--- src/controllers/jogosController.js
+++ src/controllers/jogosController.js
@@ -24,6 +24,34 @@ class JogoController {
         });
     }
 
+    static listarJogoPorCrossplatform = (req, res) => {
+        jogos.find({crossplatform: true}, (err, jogo) => {
+            if (!err){
+                if (jogo.length > 0) {
+                    res.status(200).send(jogo);
+                } else {
+                    res.status(200).send('Nenhum jogo cadastrado possui crossplatform');
+                }
+            } else {
+                res.status(400).send({mensagem: `${err.message}`});
+            }
+        });
+    }
+
+    static listarJogosMultiplayer = (req, res) => {
+        jogos.find({multiplayer: true}, (err, jogo) => {
+            if (!err){
+                if (jogo.length > 0) {
+                    res.status(200).send(jogo);
+                } else {
+                    res.status(200).send('Nenhum jogo cadastrado possui multiplayer');
+                }
+            } else {
+                res.status(400).send({mensagem: `${err.message}`});
+            }
+        });
+    }
+
     static cadastrarJogo = (req, res) => {
         let jogo = new jogos(req.body);
         jogo.save((err) => {
@@ -35,8 +63,32 @@ class JogoController {
         })
     }
 
+    static atualizarJogo = (req, res) => {
+        const id = req.params.id;
+
+        jogos.findByIdAndUpdate(id, { $set: req.body }, (err, jogo) => {
+            if (!err) {
+                res.status(200).send({ message: `${jogo.nome} atualizado com sucesso.` });
+            } else {
+                res.status(500).send({ message: `${err.message} - Erro ao atualizar o jogo.` });
+            }
+        });
+    }
+
+    static deletarJogo = (req, res) => {
+        const id = req.params.id;
+        jogos.findByIdAndDelete(id, (err, jogo) => {
+            if (!err){
+                res.status(200).send(`${jogo.nome} deletado com sucesso!`);
+            } else {
+                res.status(400).send({mensagem: `${err.message} - ID do jogo não encontrado`});
+            }
+        })
+    }
+  
 
 }
+
 
 
 
--- src/routes/jogosRoutes.js
+++ src/routes/jogosRoutes.js
@@ -5,8 +5,12 @@ const router = express.Router();
 
 router
     .get('/jogos', JogoController.listarJogos)
+    .get('/jogos/crossplatform/', JogoController.listarJogoPorCrossplatform)
+    .get('/jogos/multiplayer', JogoController.listarJogosMultiplayer)
     .get('/jogos/:id', JogoController.listarJogoPorId)
     .post('/jogos', JogoController.cadastrarJogo)
+    .put('/jogos/:id', JogoController.atualizarJogo)
+    .delete('/jogos/:id', JogoController.deletarJogo)
 
 
 export default router;
